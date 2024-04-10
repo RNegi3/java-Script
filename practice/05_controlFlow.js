c76e407b0564a9c5ae7a3e0014194eda760841e1
RNegi3 <115223802+RNegi3@users.noreply.github.com>
2024-04-10 13:02:41 +0000

    Learn more about IIFE's and conditions

---
+++ practice/05_controlFlow.js
@@ -0,0 +1,10 @@
+// const a = 2
+
+// if (a > 1){
+//     const b = 3
+//     console.log(a);
+// }
+// console.log(b); gives an error since you are really change the value of 'const' + outside of the scope {}
+
+// '===' used to check the type and the value to another value
+
